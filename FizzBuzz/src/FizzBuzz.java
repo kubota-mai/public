d945b8283dea219ab92f46fe017b87af76ea8e48
kubota-mai <kubota.s.tennis@gmail.com>
2020-02-05 21:24:39 +0900

    FizzBuzz

---
+++ FizzBuzz/src/FizzBuzz.java
@@ -0,0 +1,27 @@
+
+public class FizzBuzz {
+
+	public static void main(String[] args) {
+
+		for(int i=1;i<=100;i++) {
+			if(i%15==0) {
+				System.out.println("FizzBuzz");
+			}else if(i%3==0) {
+				System.out.println("Fizz");
+			}else if(i%5==0) {
+				System.out.println("Buzz");
+			}else {
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
+
+
+//このプログラム言語を選択した理由
+
+//作成時に工夫した点、苦労した点
+
+//作成時に参考にしたもの
